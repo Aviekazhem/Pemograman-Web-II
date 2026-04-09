afb3c34ff5802dc08aae380c7b58e617109f098f
Kazhem Avie <kazhem6avie@gmail.com>
2026-04-09 13:29:47 +0700

    Local
    BAB V  ARRAY

---
+++ Pertemuan6/Latihan1.php
@@ -0,0 +1,20 @@
+<?php 
+$a[0] =2; 
+$a[1] =5; 
+$a[2] =9;
+$a[3] =13;
+$a[4] =16;
+$jumlah =count($a); 
+print "Jumlah array a = $jumlah <br>";
+
+// variabel $jumlah akan bernilai 3 
+$b["buah"] = "semangka"; 
+$b["sayur"] ="wortel"; 
+$b["daging"] ="ayam"; 
+$b["utama"] ="nasi";
+$b["Biji-Bijian"] = "Kacang Hijau";
+$jumlah = sizeof($b); 
+print "Jumlah array b = $jumlah <br>";
+
+// variabel $jumlah akan bernilai 4 
+?>
---
+++ Pertemuan6/Latihan3.php
@@ -0,0 +1,11 @@
+<?php
+$UsiaKaryawan["Haeruddin"] = "59"; 
+$UsiaKaryawan["Ihwanto"] = "55"; 
+$UsiaKaryawan["Angga"] = "69"; 
+$UsiaKaryawan["Fajar"] = "70"; 
+$UsiaKaryawan["Iqbal"] = "56"; 
+foreach($UsiaKaryawan as $Nama => $umur) 
+{ 
+echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>"; 
+} 
+?>
